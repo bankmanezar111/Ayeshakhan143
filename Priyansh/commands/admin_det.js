0da225d85f36d42017209dbcda16b44175bfdaf7
9253Raza <razaji92535982@gmail.com>
2024-12-18 23:15:48 +0530

    Update admin_det.js

--- Priyansh/commands/admin_det.js
+++ Priyansh/commands/admin_det.js
@@ -14,12 +14,12 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("ADMIN")==0 || event.body.indexOf("Admin")==0 || event.body.indexOf("/Admin")==0 || event.body.indexOf("#admin")==0) {
     var msg = {
-        body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 🫅",
+        body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 𝐏𝐈𝐂 🫅",
         attachment: 
-fs.createReadStream(__dirname + `/noprefix/IMG_20241005_135006.jpg`)
+fs.createReadStream(__dirname + `/noprefix/Priyansh/commands/noprefix/Messenger_creation_3AA90A1A-E4E7-4F68-90A8-1D757424676E.jpeg`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🫅", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🥵", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
