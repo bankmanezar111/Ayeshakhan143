1781464d07483fe615e3244390be5a3c939928b5
9253Raza <razaji92535982@gmail.com>
2024-12-18 20:53:05 +0530

    Update 0_ayesha.js

--- Priyansh/commands/0_ayesha.js
+++ Priyansh/commands/0_ayesha.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         if (event.body.indexOf("ayesha")==0 || event.body.indexOf("AYESHA")==0 || event.body.indexOf("ayesha")==0 || event.body.indexOf("@Ayesha Khan")==0) {
                 var msg = {
                                 body: "=𝐎𝐰𝐧𝐞𝐫 ➻   KHAN SAHAB \n__________________________________\n\nAYESHA BHOT PYARI LADKI HIA   \n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/noprefix/e6dc27b014a7e2be2cc64f6a9eeed053.jpg`)
+                                attachment: fs.createReadStream(__dirname + `/noprefix/ayesha.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("☕", event.messageID, (err) => {}, true)
