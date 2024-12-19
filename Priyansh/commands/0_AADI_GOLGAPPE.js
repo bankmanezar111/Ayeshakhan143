c588014528d3376427f87e703344072187b913db
9253Raza <razaji92535982@gmail.com>
2024-12-19 22:13:11 +0530

    Update 0_AADI_GOLGAPPE.js

--- Priyansh/commands/0_AADI_GOLGAPPE.js
+++ Priyansh/commands/0_AADI_GOLGAPPE.js
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("Golgappe")==0 || event.body.indexOf("Golgappa")==0 || event.body.indexOf("Pani puri")==0 || event.body.indexOf("PANI PURI")==0) {
 		var msg = {
-				body: "☟  ========== ☟ ==========  ☟                                                         ●===========================●                              𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝘚𝘏𝘈𝘈𝘕   𝘒𝘏𝘈𝘕 === 🩷 𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐆𝐎𝐋𝐆𝐀𝐏𝐏𝐀 𝐊𝐇𝐀𝐎 🩷",
-				attachment: fs.createReadStream(__dirname + `/noprefix/Pin_on_Pins_by_you(360P)(1).mp4`)
+				body: "☟  ========== ☟ ==========  ☟                                                         ●===========================●                              𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐊𝐇𝐀𝐍    𝐒𝐀𝐇𝐀𝐁 ===     🩷 𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐆𝐎𝐋𝐆𝐀𝐏𝐏𝐀 𝐊𝐇𝐀𝐎 🩷",
+				attachment: fs.createReadStream(__dirname + `/noprefix/20cf76d509c4ca0e83883f9052454cab.gif`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😋", event.messageID, (err) => {}, true)
