371955358a73d3c66d3fa1018674d519ea8c354c
9253Raza <razaji92535982@gmail.com>
2024-12-19 20:48:51 +0530

    Update 0_AADI-PEPSI.js

--- Priyansh/commands/0_AADI-PEPSI.js
+++ Priyansh/commands/0_AADI-PEPSI.js
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("pepsi")==0 || event.body.indexOf("Pepsi")==0 || event.body.indexOf("PEPSI")==0 || event.body.indexOf("Coldaring")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐄𝐏𝐒𝐈\n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/noprefix/Pin_di_Shoaib_nazir_su_Quick_Saves(360P).mp4`)
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐊𝐇𝐀𝐍  𝐒𝐀𝐇𝐀𝐁 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐄𝐏𝐒𝐈\n__________________________________ ",
+                                attachment: fs.createReadStream(__dirname + `/noprefix/ef4be9bf2788696e56231c33920bf54f.gif`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🥂", event.messageID, (err) => {}, true)
