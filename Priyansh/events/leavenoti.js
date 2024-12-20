5a4acae8fb75b7326b4e18ac6f1aff7a6ab44589
9253Raza <razaji92535982@gmail.com>
2024-12-20 22:05:01 +0530

    Update leavenoti.js

--- Priyansh/events/leavenoti.js
+++ Priyansh/events/leavenoti.js
@@ -40,7 +40,7 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-(typeof data.customLeave == "undefined") ? msg = "𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐓𝐨  𝐊𝐡𝐚𝐧 𝐒𝐚𝐡𝐚𝐛  𝐁𝐨𝐭 😇👈 ●============================●  [⚜️] •💜• === {name} === •💜• ●============================● KO Bhaga diya 🌝 ●============================● 𝐀𝐛 𝐔𝐬𝐤𝐞 𝐁𝐢𝐧𝐚 𝐊𝐞𝐬𝐞 𝐌𝐚𝐧 𝐋𝐚𝐠𝐞𝐠𝐚 𝐌𝐞𝐫𝐚🥺🥺🥺 •💜• {type}  [⚜️] ●============================● 𝙃𝘼𝙈 𝙒𝙊 𝙃𝘼𝙄 𝙅𝙉𝘼𝘼𝘽 𝙅𝙊 𝘽𝘼𝘼𝙏 𝙎𝙀 𝙅𝘼𝘼𝙏 𝙊𝙍 𝙃𝘼𝙍𝙆𝙏𝙊 𝙎𝙀 𝙊𝙆𝘼𝘼𝙏 𝙉𝘼𝘼𝙋 𝙇𝙀𝙏𝙀 𝙃𝘼𝙄😎😎\n😒 𝐀𝐀𝐏𝐊𝐀 𝐀𝐏𝐍𝐀 (𝐎𝐰𝐧𝐞𝐫) ➻  ────  𝐊𝐡𝐚𝐧 𝐒𝐚𝐡𝐚𝐛 😒\n●============================● •💜• {name} •💜• ●============================● •💜• BEHTI HAWA SA THAA WO 😥 uDTI PATANG✨✨ SAA THAA WOO ♥ KAHA GAYA USE DHOONDHO🤔🤔🤔 ●============================● •💜•\n\n[❤️‍🔥] 🖤🖤😥😥...Good {session} || {time}" : msg = data.customLeave;
+(typeof data.customLeave == "undefined") ? msg = "𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐓𝐨  𝐊𝐡𝐚𝐧 𝐒𝐚𝐡𝐚𝐛  𝐁𝐨𝐭 😇👈 ●============================●  [⚜️] •💜• === {name} === •💜• ●============================● KO Bhaga diya 🌝 ●============================● 𝐊𝐢𝐭𝐧𝐚 𝐐𝐮𝐭𝐞 𝐓𝐡𝐚 𝐍𝐚 𝐘𝐞 𝐀𝐛 𝐔𝐬𝐤𝐞 𝐁𝐢𝐧𝐚 𝐊𝐞𝐬𝐞 𝐌𝐚𝐧 𝐋𝐚𝐠𝐞𝐠𝐚 𝐌𝐞𝐫𝐚🥺🥺🥺 •💜• {type}  [⚜️] ●============================● 𝙃𝘼𝙈 𝙒𝙊 𝙃𝘼𝙄 𝙅𝙉𝘼𝘼𝘽 𝙅𝙊 𝘽𝘼𝘼𝙏 𝙎𝙀 𝙅𝘼𝘼𝙏 𝙊𝙍 𝙃𝘼𝙍𝙆𝙏𝙊 𝙎𝙀 𝙊𝙆𝘼𝘼𝙏 𝙉𝘼𝘼𝙋 𝙇𝙀𝙏𝙀 𝙃𝘼𝙄😎😎\n😒 𝐀𝐀𝐏𝐊𝐀 𝐀𝐏𝐍𝐀 (𝐎𝐰𝐧𝐞𝐫) ➻  ────  𝐊𝐡𝐚𝐧 𝐒𝐚𝐡𝐚𝐛 😒\n●============================● •💜• {name} •💜• ●============================● •💜• BEHTI HAWA SA THAA WO 😥 uDTI PATANG✨✨ SAA THAA WOO ♥ KAHA GAYA USE DHOONDHO🤔🤔🤔 ●============================● •💜•\n\n[❤️‍🔥] 🖤🖤😥😥...Good {session} || {time}" : msg = data.customLeave;
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type).replace(/\{session}/g, hours <= 10 ? "𝙈𝙤𝙧𝙣𝙞𝙣𝙜" : 
     hours > 10 && hours <= 12 ? "𝘼𝙛𝙩𝙚𝙧𝙉𝙤𝙤𝙣" :
     hours > 12 && hours <= 18 ? "𝙀𝙫𝙚𝙣𝙞𝙣𝙜" : "𝙉𝙞𝙜𝙝𝙩").replace(/\{time}/g, time);  
