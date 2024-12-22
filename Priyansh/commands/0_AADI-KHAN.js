6e8529776fd3ed18c345134e86b9a374bbe96536
9253Raza <razaji92535982@gmail.com>
2024-12-22 13:28:58 +0530

    Update 0_AADI-KHAN.js

--- Priyansh/commands/0_AADI-KHAN.js
+++ Priyansh/commands/0_AADI-KHAN.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         if (event.body.indexOf("jaan")==0 || event.body.indexOf("Jaan")==0 || event.body.indexOf("Janu")==0 || event.body.indexOf("jan")==0) {
                 var msg = {
                                 body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐘𝐄𝐒𝐇𝐀 𝐊𝐄 𝐇𝐀𝐒𝐁𝐄𝐍𝐃 𝐉𝐈😍 \n__________________________________\n\n𝐀𝐘𝐄𝐒𝐇𝐀 𝐊𝐇𝐀𝐍 𝐘𝐄 𝐑𝐇𝐈 𝐀𝐀𝐏𝐊𝐈 𝐉𝐀𝐀𝐍👇👇👇\n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/noprefix/received_3821532368175954.jpeg`)
+                                attachment: fs.createReadStream(__dirname + `/noprefix/Messenger_creation_DF8CFAEC-A51D-4752-B15D-B98852223EB1.jpeg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😳", event.messageID, (err) => {}, true)
